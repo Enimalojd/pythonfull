cc28ae49d7c5d39ff367709b158f328c0f1f7093
Enimalojd <enderston8@gmail.com>
2023-10-18 16:31:23 +0400

    add register serializer

---
+++ src/core/auth/serializers/register.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from core.user.serializers import UserSerializer
+from core.user.models import User
+
+
+class RegisterSerializer(UserSerializer):
+    '''Сериализатор регистрации новых пользователей'''
+    # пароль должен быть минимум 8 символов, но не больше 128 символов
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'bio', 'avatar', 'email',
+                  'username', 'first_name', 'last_name',
+                  'password']
+
+        def create(self, validated_data):
+            #Используем тот же метод для создания пользователя, который использовали в UserManager
+
+            return User.objects.create_user(**validated_data)
