0796eb76c083f00df7ceaa2a41e4a73d796095be
Enimalojd <enderston8@gmail.com>
2023-10-18 20:49:33 +0400

    refactoring

--- src/core/user/models.py
+++ src/core/user/models.py
@@ -4,14 +4,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.http import Http404
 
+from core.abstract.models import AbstractManager, AbstractModel
 
-class UserManager(BaseUserManager):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
+
+class UserManager(BaseUserManager, AbstractManager):
 
     def create_user(self, username, email, password=None, **kwargs):
         """Создаёт и возвращает Пользователя с почтой, телефоном, юзернеймом и паролем"""
@@ -47,9 +43,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True,
-                                 default=uuid.uuid4, editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True,
                                 max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
@@ -57,8 +51,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
     bio = models.TextField(blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
