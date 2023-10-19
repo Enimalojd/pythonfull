9b84eec67534d8fd3626c635d2084797714d6709
Enimalojd <enderston8@gmail.com>
2023-10-19 20:42:57 +0400

    update comments

--- src/core/comment/serializers.py
+++ src/core/comment/serializers.py
@@ -20,12 +20,24 @@ class CommentSerializer(AbstractSerializer):
                 raise ValidationError("Нельзя оставлять комментарии от имени других пользователей.")
         return value
 
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        return value
+
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = User.objects.get_object_by_public_id(rep['author'])
         rep['author'] = UserSerializer(author).data
 
         return rep
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+        instance = super().update(instance, validated_data)
+
+        return instance
 
     class Meta:
         model = Comment
